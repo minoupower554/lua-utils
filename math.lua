5197638a71f7ac8c402fbc82c2827dba1a4ea991
minoupower554 <minoupower554@gmail.com>
2026-04-29 02:12:24 +0200

    fix: fixed clamp incorrectly checking argument type before applying defaults

--- math.lua
+++ math.lua
@@ -21,6 +21,8 @@ end
 ---@param max number? the maximum the value is allowed to be, defaults to 1
 ---@return number result
 function m.clamp(v, min, max)
+    min = min or 0
+    max = max or 1
     if type(v) ~= "number" then
         error("the value has to be a number", 2)
     end
@@ -30,8 +32,6 @@ function m.clamp(v, min, max)
     if type(max) ~= "number" then
         error("the max value has to be a number", 2)
     end
-    min = min or 0
-    max = max or 1
     if min>max then
         error("the minimum cannot be larger than the maximum", 2)
     end
