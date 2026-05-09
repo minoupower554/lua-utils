aa85fdda38c577618748781d239322b2e7c101ab
minoupower554 <minoupower554@gmail.com>
2026-05-09 13:07:53 +0200

    feat!: removed the collect method and replaced it with a prelude

--- init.lua
+++ init.lua
@@ -1,59 +1,10 @@
----@diagnostic disable: different-requires
-local source = debug.getinfo(1).source
-local dir = source:match("@(.*/)")
+local types = require('lua-utils.types')
+local table = require('lua-utils.table')
 
-
-local function register_module(registry, name, mod)
-    if registry[name] ~= nil then
-        error("fatal: namespace registry name collision. this is an internal library error. please report any occurrences")
-    end
-    -- register the namespace
-    registry[name] = mod
-
-    -- register all functions directly
-    for k, v in pairs(mod) do
-        if type(v) ~= "table" then
-            if registry[k] ~= nil then
-                error("fatal: function registry name collision. this is an internal library error. please report any occurrences")
-            end
-            registry[k] = v
-        end
-    end
-end
-
-local old_path = package.path
-package.path = table.concat({
-    dir .. "?.lua",
-    dir .. "?/init.lua",
-    package.path
-}, ";")
--- do as little as possible in the timeframe where it pokes package.path
-local math = require('math')
-local string = require('string')
-local types = require('types')
-local misc = require('misc')
-
-package.path = old_path
-
-local registry = {}
-register_module(registry, "math", math)
-register_module(registry, "string", string)
-register_module(registry, "types", types)
-register_module(registry, "misc", misc)
-
-
----collect any functions you might want dynamically, also allows you to collect namespaces
----@param ... string
-return function(...)
-    local vararg = {...}
-
-    local acc = {}
-    for k, v in pairs(vararg) do
-        if registry[k] == nil then
-            error("invalid import: "..k, 2)
-        end
-        table.insert(acc, registry[v])
-    end
-
-    return table.unpack(acc)
-end
+---return a prelude of components
+return {
+    null=types.null,
+    Option=types.Option,
+    Result=types.Result,
+    pprint=table.pprint
+}
